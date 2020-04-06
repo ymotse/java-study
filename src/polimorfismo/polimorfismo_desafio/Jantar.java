42634ef1c13d2f78064e0c4dcf469240bcda6d16
Yitshhaq Fukushima <ymotse@gmail.com>
2020-04-06 17:13:59 -0300

    Desafio Polimorfismo dinamico.

---
+++ src/polimorfismo/polimorfismo_desafio/Jantar.java
@@ -0,0 +1,24 @@
+package polimorfismo.polimorfismo_desafio;
+
+public class Jantar {
+	
+	public static void main(String[] args) {
+		
+		Pessoa convidado = new Pessoa(99.65);
+		
+		Arroz arroz = new Arroz(0.1);
+		Feijao feijao = new Feijao(0.2);
+		
+		convidado.comer(arroz);
+		convidado.comer(feijao);
+
+		System.out.println(convidado.getPeso());
+		
+		Sorvete sorteve = new Sorvete(0.4);
+		convidado.comer(sorteve);
+		
+		System.out.println(convidado.getPeso());
+		
+	}
+	
+}
