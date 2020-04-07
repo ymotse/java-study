0e32cfcf7b2e69c524be13abb8e5b56abf469174
Yitshhaq Fukushima <ymotse@gmail.com>
2020-04-07 18:37:43 -0300

    Lambda [1] com Interface.

---
+++ src/lambdas/CalculoTeste2.java
@@ -0,0 +1,16 @@
+package lambdas;
+
+public class CalculoTeste2 {
+
+	public static void main(String[] args) {
+		
+		Calculo calcular = (x, y) -> {
+			return x + y;
+		};
+		System.out.println(calcular.executar(2, 3));
+		
+		calcular = (x, y) -> x * y;
+		System.out.println(calcular.executar(2, 3));
+	}
+	
+}
