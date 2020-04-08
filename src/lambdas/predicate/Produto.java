dedc24522fc6dc4c40df5a12335fdfdf982470ab
Yitshhaq Fukushima <ymotse@gmail.com>
2020-04-08 01:01:47 -0300

    Predicate.

---
+++ src/lambdas/predicate/Produto.java
@@ -0,0 +1,16 @@
+package lambdas.predicate;
+
+public class Produto extends Object {
+
+	final String nome;
+	final double preco;
+	final double desconto;
+
+	public Produto(String nome, double preco, double desconto) {
+		super();
+		this.nome = nome;
+		this.preco = preco;
+		this.desconto = desconto;
+	}
+
+}
