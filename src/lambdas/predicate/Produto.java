cf996d946e3ea4ebbb7f4290743cef8b7815112a
Yitshhaq Fukushima <ymotse@gmail.com>
2020-04-08 01:12:13 -0300

    Consumer.

--- src/lambdas/predicate/Produto.java
+++ src/lambdas/predicate/Produto.java
@@ -2,15 +2,20 @@ package lambdas.predicate;
 
 public class Produto extends Object {
 
-	final String nome;
-	final double preco;
-	final double desconto;
+	public final String nome;
+	public final double preco;
+	public final double desconto;
 
 	public Produto(String nome, double preco, double desconto) {
 		super();
 		this.nome = nome;
 		this.preco = preco;
 		this.desconto = desconto;
+	}
+	
+	public String toString() {
+		double precoFinal = preco * (1 - desconto);
+		return nome + " tem preco de R$ " + precoFinal;
 	}
 
 }
