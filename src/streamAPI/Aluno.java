31a58edb0860a7a4ac9bffa3850aae69471564f5
Yitshhaq Fukushima <ymotse@gmail.com>
2020-04-10 16:00:00 -0300

    Reduce [2].

--- src/streamAPI/Aluno.java
+++ src/streamAPI/Aluno.java
@@ -6,8 +6,11 @@ public class Aluno {
 	final double nota;
 	final boolean bomComportamento;
 	
+	public Aluno(String nome, double nota) {
+		this(nome, nota, true);
+	}
+	
 	public Aluno(String nome, double nota, boolean bomComportamento) {
-		super();
 		this.nome = nome;
 		this.nota = nota;
 		this.bomComportamento = bomComportamento;
