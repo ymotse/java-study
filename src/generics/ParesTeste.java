82ef2350f84456f9b968fffca816bcf61663bcb5
Yitshhaq Fukushima <ymotse@gmail.com>
2020-04-25 11:19:41 -0300

    Estrutura Chave/Valor com Generics.

---
+++ src/generics/ParesTeste.java
@@ -0,0 +1,21 @@
+package generics;
+
+public class ParesTeste {
+	
+	public static void main(String[] args) {
+		
+		Pares<Integer, String> resultadoConcurso = new Pares<>();
+		resultadoConcurso.adicionar(1, "Maria");
+		resultadoConcurso.adicionar(2, "Pedro");
+		resultadoConcurso.adicionar(3, "Gui");
+		resultadoConcurso.adicionar(4, "Ana");
+		resultadoConcurso.adicionar(2, "Rebeca");
+		resultadoConcurso.adicionar(6, "Paulo");
+		
+		System.out.println(resultadoConcurso.getValor(1));
+		System.out.println(resultadoConcurso.getValor(2));
+		System.out.println(resultadoConcurso.getValor(4));
+		
+	}
+	
+}
