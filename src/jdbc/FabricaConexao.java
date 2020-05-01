0e64b4bd4c0121fbad3d433148332d9d6a759cb1
Yitshhaq Fukushima <ymotse@gmail.com>
2020-05-01 20:42:07 -0300

    Classe para conexao com Banco de Dados MySQL.

---
+++ src/jdbc/FabricaConexao.java
@@ -0,0 +1,21 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class FabricaConexao {
+
+	public static Connection getConexao() {
+		try {
+			final String url = "jdbc:mysql://localhost:3308?verifyServerCertificate=false&useSSL=true";
+			final String usuario = "root";
+			final String senha = "ymotse";
+			
+			return DriverManager.getConnection(url, usuario, senha);
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+}
