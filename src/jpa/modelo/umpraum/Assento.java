09807f593a68c159278e2ae568e5d49319c74660
Yitshhaq Fukushima <ymotse@gmail.com>
2020-05-02 03:06:24 -0300

    Mapeamento bidirecional um para um.

--- src/jpa/modelo/umpraum/Assento.java
+++ src/jpa/modelo/umpraum/Assento.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -15,6 +16,9 @@ public class Assento {
 	private Long id;
 	
 	private String nome;
+	
+	@OneToOne(mappedBy = "assento")
+	private Cliente cliente;
 	
 	public Assento() {
 	
@@ -39,6 +43,14 @@ public class Assento {
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
 	}
 	
 }
