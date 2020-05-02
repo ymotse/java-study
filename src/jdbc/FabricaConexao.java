195b647a74cd82047859682601eda139c056c5dd
Yitshhaq Fukushima <ymotse@gmail.com>
2020-05-01 21:39:01 -0300

    Externalizar dados de Conexão.

--- src/jdbc/FabricaConexao.java
+++ src/jdbc/FabricaConexao.java
@@ -1,21 +1,34 @@
 package jdbc;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class FabricaConexao {
 
 	public static Connection getConexao() {
 		try {
-			final String url = "jdbc:mysql://localhost:3308/curso_java?verifyServerCertificate=false&useSSL=true";
-			final String usuario = "root";
-			final String senha = "ymotse";
+			Properties props = getProperties();
+			
+			final String url = props.getProperty("banco.url");
+			final String usuario = props.getProperty("banco.usuario");
+			final String senha = props.getProperty("banco.senha");
 			
 			return DriverManager.getConnection(url, usuario, senha);
-		} catch (SQLException e) {
+		} catch (SQLException | IOException e) {
 			throw new RuntimeException(e);
 		}
+	}
+	
+	
+	private static Properties getProperties() throws IOException {
+		String caminho = "/conexao.properties";
+
+		Properties props = new Properties();
+		props.load(FabricaConexao.class.getResourceAsStream(caminho));
+		return props;
 	}
 	
 }
