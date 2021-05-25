386be61d1814fea950f0a6f335c225f65d2a11df
Yitshhaq Fukushima <ymotse@gmail.com>
2021-05-25 09:26:31 -0300

    [contactEjb] - Mock Unit tests in save Contact #2

--- contactEjb/src/test/java/com/example/dao/ContactDAOTest.java
+++ contactEjb/src/test/java/com/example/dao/ContactDAOTest.java
@@ -7,6 +7,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.mockito.ArgumentMatchers;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
@@ -21,26 +22,41 @@ public class ContactDAOTest extends TestCase {
 
     @Mock
     private EntityManager em;
-
+    
+    ContactDAO contactDAO;
+    
     @Before
     public void init() {
         MockitoAnnotations.initMocks(this);
 
         em = Mockito.mock(EntityManager.class);
+
+        contactDAO = new ContactDAO(em);
     }
 
     @Test
     public void persist_returnsContact_whenSuccessful() throws Exception {
-
         Contact contactToBeSaved = ContactCreator.createContactToBeSaved();
 
         Mockito.doNothing().when(em).persist(contactToBeSaved);
 
-        ContactDAO contactDAO = new ContactDAO(em);
         Contact contactSaved = contactDAO.save(contactToBeSaved);
 
         Assert.assertNotNull(contactSaved);
         Assert.assertEquals(contactToBeSaved.getName(), contactSaved.getName());
     }
-
+    
+    @Test
+    public void persist_returnsNull_whenIdIsNull() throws Exception {
+        Assert.assertNull(contactDAO.save(ContactCreator.createContactToBeValidate()));
+    }
+    
+    @Test(expected = NullPointerException.class)
+    public void persist_throwsNullPointerException_whenContactIsNull() {
+        Mockito.doThrow(new NullPointerException()).when(em).persist(ArgumentMatchers.any(Contact.class));
+        
+        Assert.assertEquals(new NullPointerException(), contactDAO.save(new Contact()));
+        Assert.assertNull(contactDAO.save(null));
+    }
+    
 }
