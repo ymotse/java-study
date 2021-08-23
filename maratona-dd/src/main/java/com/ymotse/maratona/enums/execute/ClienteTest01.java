2061d253ec9d6a1878200887ed83d502fbdc207e
Yitshhaq Fukushima <ymotse@gmail.com>
2021-08-23 19:02:49 -0300

    [maratona-dd] - Enumeração - 02 - Construtores e atributos

--- maratona-dd/src/main/java/com/ymotse/maratona/enums/execute/ClienteTest01.java
+++ maratona-dd/src/main/java/com/ymotse/maratona/enums/execute/ClienteTest01.java
@@ -6,15 +6,11 @@ import com.ymotse.maratona.enums.dominio.TipoCliente;
 public class ClienteTest01 {
 
     public static void main(String[] args) {
-        Cliente cliente1 = new Cliente("Buma", TipoCliente.PESSOA_FISICA);
-        Cliente cliente2 = new Cliente("Mestre Kame", TipoCliente.PESSOA_JURIDICA);
-        Cliente cliente3 = new Cliente("Goku", TipoCliente.PESSOA_FISICA);
-        Cliente cliente4 = new Cliente("Kuririn", TipoCliente.PESSOA_JURIDICA);
+        Cliente cliente1 = new Cliente("Buma", TipoCliente.PESSOA_FISICA, Cliente.TipoPagamento.DEBITO);
+        Cliente cliente2 = new Cliente("Mestre Kame", TipoCliente.PESSOA_JURIDICA, Cliente.TipoPagamento.CREDITO);
 
         System.out.println(cliente1);
         System.out.println(cliente2);
-        System.out.println(cliente3);
-        System.out.println(cliente4);
     }
 
 }
