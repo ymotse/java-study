8b92c22472ed0d510b30587ebb1b912de2357adb
Yitshhaq Fukushima <ymotse@gmail.com>
2021-08-23 11:19:18 -0300

    [maratona-dd] - Herança 02 - Super

--- maratona-dd/src/main/java/com/ymotse/maratona/heranca/dominio/Funcionario.java
+++ maratona-dd/src/main/java/com/ymotse/maratona/heranca/dominio/Funcionario.java
@@ -3,6 +3,11 @@ package com.ymotse.maratona.heranca.dominio;
 public class Funcionario extends Pessoa {
     private double salario;
 
+    public void imprimir() {
+        super.imprimir();
+        System.out.println(this.salario);
+    }
+
     public double getSalario() {
         return salario;
     }
