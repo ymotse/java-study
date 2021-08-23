10845e4d912181d8f94ff5c6d7f1798ef63ee0f0
Yitshhaq Fukushima <ymotse@gmail.com>
2021-08-23 13:00:04 -0300

    [maratona-dd] - Modificador final - 02 - Tipo referencia

--- maratona-dd/src/main/java/com/ymotse/maratona/modificadorFinal/dominio/Carro.java
+++ maratona-dd/src/main/java/com/ymotse/maratona/modificadorFinal/dominio/Carro.java
@@ -4,6 +4,7 @@ public class Carro {
     private String nome;
 //    private static final double VELOCIDADE_LIMITE = 250;
     public static final double VELOCIDADE_LIMITE;
+    public final Comprador COMPRADOR = new Comprador();
 
     static {
         VELOCIDADE_LIMITE = 250;
@@ -16,4 +17,5 @@ public class Carro {
     public void setNome(String nome) {
         this.nome = nome;
     }
+
 }
--- maratona-dd/src/main/java/com/ymotse/maratona/modificadorFinal/execute/CarroTest01.java
+++ maratona-dd/src/main/java/com/ymotse/maratona/modificadorFinal/execute/CarroTest01.java
@@ -1,12 +1,18 @@
 package com.ymotse.maratona.modificadorFinal.execute;
 
 import com.ymotse.maratona.modificadorFinal.dominio.Carro;
+import com.ymotse.maratona.modificadorFinal.dominio.Comprador;
 
 public class CarroTest01 {
 
     public static void main(String[] args) {
         Carro carro = new Carro();
+
         System.out.println(Carro.VELOCIDADE_LIMITE);
+        System.out.println(carro.COMPRADOR);
+
+        carro.COMPRADOR.setNome("Kuririn");
+        System.out.println(carro.COMPRADOR);
     }
 
 }
