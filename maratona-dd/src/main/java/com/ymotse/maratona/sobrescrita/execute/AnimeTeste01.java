e9a087df255d90d4d2ace946180281bfd9c425b6
Yitshhaq Fukushima <ymotse@gmail.com>
2021-08-23 12:52:52 -0300

    [maratona-dd] - Sobrescrita do método toString

---
+++ maratona-dd/src/main/java/com/ymotse/maratona/sobrescrita/execute/AnimeTeste01.java
@@ -0,0 +1,12 @@
+package com.ymotse.maratona.sobrescrita.execute;
+
+import com.ymotse.maratona.sobrescrita.dominio.Anime;
+
+public class AnimeTeste01 {
+
+    public static void main(String[] args) {
+        Anime anime = new Anime("Dr. Stone");
+        System.out.println(anime);
+    }
+
+}
