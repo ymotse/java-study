245d69851ad0fbe98ce6a9c673eef2de95a49da4
Yitshhaq Fukushima <ymotse@gmail.com>
2021-08-25 11:03:10 -0300

    [maratona-dd] - Polimorfismo - 03 - Parâmetros polimórficos

--- maratona-dd/src/main/java/com/ymotse/maratona/polimorfismo/execute/ProdutoTest01.java
+++ maratona-dd/src/main/java/com/ymotse/maratona/polimorfismo/execute/ProdutoTest01.java
@@ -1,6 +1,7 @@
 package com.ymotse.maratona.polimorfismo.execute;
 
 import com.ymotse.maratona.polimorfismo.dominio.Computador;
+import com.ymotse.maratona.polimorfismo.dominio.Televisao;
 import com.ymotse.maratona.polimorfismo.dominio.Tomate;
 import com.ymotse.maratona.polimorfismo.servico.CalculadoraImposto;
 
@@ -9,11 +10,15 @@ public class ProdutoTest01 {
     public static void main(String[] args) {
         Computador computador = new Computador("NUC10i7", 10000);
         Tomate tomate = new Tomate("Tomate Siciliano", 10);
+        Televisao tv = new Televisao("Samsung 50\"", 5000);
 
-        CalculadoraImposto.calcularImpostoComputador(computador);
+        CalculadoraImposto.calcularImposto(computador);
         System.out.println();
-        CalculadoraImposto.calcularImpostoTomate(tomate);
 
+        CalculadoraImposto.calcularImposto(tomate);
+        System.out.println();
+
+        CalculadoraImposto.calcularImposto(tv);
     }
 
 }
--- maratona-dd/src/main/java/com/ymotse/maratona/polimorfismo/servico/CalculadoraImposto.java
+++ maratona-dd/src/main/java/com/ymotse/maratona/polimorfismo/servico/CalculadoraImposto.java
@@ -1,23 +1,14 @@
 package com.ymotse.maratona.polimorfismo.servico;
 
-import com.ymotse.maratona.polimorfismo.dominio.Computador;
-import com.ymotse.maratona.polimorfismo.dominio.Tomate;
+import com.ymotse.maratona.polimorfismo.dominio.Produto;
 
 public class CalculadoraImposto {
 
-    public static void calcularImpostoComputador(Computador computador) {
-        System.out.println("Relatorio de imposto do Computador");
-        double imposto = computador.calcularImposto();
-        System.out.println("Computador: " + computador.getNome());
-        System.out.println("Valor: " + computador.getValor());
-        System.out.println("Imposto a ser pago: " + imposto);
-    }
-
-    public static void calcularImpostoTomate(Tomate tomate) {
-        System.out.println("Relatorio de imposto do Tomate");
-        double imposto = tomate.calcularImposto();
-        System.out.println("Tomate: " + tomate.getNome());
-        System.out.println("Valor: " + tomate.getValor());
+    public static void calcularImposto(Produto produto) {
+        System.out.println("Relatorio de imposto");
+        double imposto = produto.calcularImposto();
+        System.out.println("Produto: " + produto.getNome());
+        System.out.println("Valor: " + produto.getValor());
         System.out.println("Imposto a ser pago: " + imposto);
     }
 
