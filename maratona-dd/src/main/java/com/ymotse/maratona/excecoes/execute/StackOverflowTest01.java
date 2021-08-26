2b1dee41568d37139eb05a9bf06675ddb31d3903
Yitshhaq Fukushima <ymotse@gmail.com>
2021-08-26 09:59:07 -0300

    [maratona-dd] - Exceções - 01 - Errors

---
+++ maratona-dd/src/main/java/com/ymotse/maratona/excecoes/execute/StackOverflowTest01.java
@@ -0,0 +1,13 @@
+package com.ymotse.maratona.excecoes.execute;
+
+public class StackOverflowTest01 {
+
+    public static void main(String[] args) {
+//        recursividade();
+    }
+
+    public static void recursividade() {
+        recursividade();
+    }
+
+}
