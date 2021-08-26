318933fec7d51fe2820499467635725e68a79af0
Yitshhaq Fukushima <ymotse@gmail.com>
2021-08-26 12:24:41 -0300

    [maratona-dd] - Exceções - 05 - Lançando exceção checked

---
+++ maratona-dd/src/main/java/com/ymotse/maratona/excecoes/execute/ExceptionTest02.java
@@ -0,0 +1,24 @@
+package com.ymotse.maratona.excecoes.execute;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ExceptionTest02 {
+
+    public static void main(String[] args) throws IOException {
+        criarNovoArquivo();
+    }
+
+    private static void criarNovoArquivo() throws IOException {
+        File file = new File("arquivo\\teste.txt");
+        try {
+            boolean isCriado = file.createNewFile();
+            System.out.println("Arquivo criado: " + isCriado);
+        } catch (IOException e) {
+            e.printStackTrace();
+//            throw new RuntimeException("Problema ao criar arquivo.");
+            throw e;
+        }
+    }
+
+}
