b9222d28d51fa6b7b593ee1af29d19d55fb5d812
Yitshhaq Fukushima <ymotse@gmail.com>
2021-08-30 11:44:35 -0300

    [maratona-dd] - Classes Utilitárias - Instant

---
+++ maratona-dd/src/main/java/com/ymotse/maratona/dates/execute/InstantTest01.java
@@ -0,0 +1,14 @@
+package com.ymotse.maratona.dates.execute;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+
+public class InstantTest01 {
+
+    public static void main(String[] args) {
+        Instant now = Instant.now();
+        System.out.println(now);
+        System.out.println(LocalDateTime.now());
+    }
+
+}
