aae56a57a7628ef0752d8672bbaf4a34f52119a2
Yitshhaq Fukushima <ymotse@gmail.com>
2021-08-30 12:55:40 -0300

    [maratona-dd] - Classes Utilitárias - TemporalAdjusters

---
+++ maratona-dd/src/main/java/com/ymotse/maratona/dates/execute/TemporalAdjustersTest01.java
@@ -0,0 +1,36 @@
+package com.ymotse.maratona.dates.execute;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.temporal.ChronoField;
+import java.time.temporal.TemporalAdjusters;
+
+public class TemporalAdjustersTest01 {
+
+    public static void main(String[] args) {
+        LocalDate now = LocalDate.now();
+//        now = now.withDayOfMonth(20);
+//        now = now.withMonth(Month.JULY.getValue());
+//        now = now.with(ChronoField.DAY_OF_MONTH, 20);
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+
+        now = LocalDate.now().with(TemporalAdjusters.nextOrSame(DayOfWeek.MONDAY));
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+
+        now = LocalDate.now().with(TemporalAdjusters.firstDayOfMonth());
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+
+        now = LocalDate.now().with(TemporalAdjusters.lastDayOfMonth());
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+
+        now = LocalDate.now().with(TemporalAdjusters.firstDayOfNextYear());
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+    }
+
+}
