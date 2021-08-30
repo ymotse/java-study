34492267b65ce40280834f63c97714ea510aad1a
Yitshhaq Fukushima <ymotse@gmail.com>
2021-08-30 10:35:54 -0300

    [maratona-dd] - Classes Utilitárias - LocalDate

---
+++ maratona-dd/src/main/java/com/ymotse/maratona/dates/execute/LocalDateTest01.java
@@ -0,0 +1,41 @@
+package com.ymotse.maratona.dates.execute;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.temporal.ChronoField;
+import java.util.Calendar;
+import java.util.Date;
+
+public class LocalDateTest01 {
+
+    public static void main(String[] args) {
+        System.out.println(new Date());
+        System.out.println(Calendar.getInstance());
+        System.out.println("------------");
+
+        LocalDate date = LocalDate.of(2021, Month.AUGUST, 29);
+        System.out.println(date.getYear());
+        System.out.println(date.getMonth());
+        System.out.println(date.getDayOfMonth());
+        System.out.println(date.getDayOfWeek());
+        System.out.println(date.getDayOfWeek().getValue());
+        System.out.println(date.lengthOfMonth());
+        System.out.println("Ano bissexto: " + date.isLeapYear());
+        System.out.println(date.get(ChronoField.YEAR));
+        System.out.println(date.get(ChronoField.DAY_OF_MONTH));
+        System.out.println(date);
+        System.out.println("------------");
+
+        LocalDate agora = LocalDate.now();
+        System.out.println(agora);
+        agora = agora.plusWeeks(4); //Immutable
+        System.out.println(agora);
+        System.out.println("------------");
+
+        System.out.println(LocalDate.MIN);
+        System.out.println(LocalDate.MAX);
+
+
+    }
+
+}
