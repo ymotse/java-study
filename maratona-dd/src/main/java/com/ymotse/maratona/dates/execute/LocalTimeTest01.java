8f2e918a102f4cf52a580e9aecfb8d0832df814c
Yitshhaq Fukushima <ymotse@gmail.com>
2021-08-30 11:11:48 -0300

    [maratona-dd] - Classes Utilitárias - LocalTime

---
+++ maratona-dd/src/main/java/com/ymotse/maratona/dates/execute/LocalTimeTest01.java
@@ -0,0 +1,27 @@
+package com.ymotse.maratona.dates.execute;
+
+import java.time.LocalTime;
+import java.time.temporal.ChronoField;
+
+public class LocalTimeTest01 {
+
+    public static void main(String[] args) {
+        LocalTime time = LocalTime.of(23, 32, 12);
+        LocalTime timeNow = LocalTime.now();
+        System.out.println(time);
+        System.out.println(timeNow);
+        System.out.println("-----------------");
+
+        System.out.println(time.getHour());
+        System.out.println(time.getMinute());
+        System.out.println(time.getSecond());
+        System.out.println(time.get(ChronoField.HOUR_OF_DAY));
+        System.out.println(time.get(ChronoField.CLOCK_HOUR_OF_AMPM));
+        System.out.println("-----------------");
+
+        System.out.println(LocalTime.MIDNIGHT);
+        System.out.println(LocalTime.MIN);
+        System.out.println(LocalTime.MAX);
+    }
+
+}
