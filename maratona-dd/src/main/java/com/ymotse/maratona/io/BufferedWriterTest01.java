e933d53a76724a9f71a42c9cfdc268b7d1495c56
Yitshhaq Fukushima <ymotse@gmail.com>
2021-09-02 14:27:32 -0300

    [maratona-dd] - Classes Utilitárias - IO - 04 - BufferedWriter

---
+++ maratona-dd/src/main/java/com/ymotse/maratona/io/BufferedWriterTest01.java
@@ -0,0 +1,23 @@
+package com.ymotse.maratona.io;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class BufferedWriterTest01 {
+
+    public static void main(String[] args) {
+        File file = new File("file.txt");
+        try(FileWriter fw = new FileWriter(file, true);
+            BufferedWriter bw = new BufferedWriter(fw)
+        ) {
+            bw.write("DevDojo - Melhor curso do Brasil!!!");
+            bw.newLine();
+            bw.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
