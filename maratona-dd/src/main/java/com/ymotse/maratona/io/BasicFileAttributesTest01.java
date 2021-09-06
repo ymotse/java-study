859915918af27f81d3c0f25a6f5d506d0ff46f49
Yitshhaq Fukushima <ymotse@gmail.com>
2021-09-06 13:03:01 -0300

    [maratona-dd] - Classes Utilitárias - NIO - 06 - BasicFileAttributes - 01

---
+++ maratona-dd/src/main/java/com/ymotse/maratona/io/BasicFileAttributesTest01.java
@@ -0,0 +1,34 @@
+package com.ymotse.maratona.io;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.FileTime;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+public class BasicFileAttributesTest01 {
+
+    public static void main(String[] args) throws IOException {
+        // BasicFileAttributes, DosFileAttributes, PosixFileAttributes
+
+        LocalDateTime date = LocalDateTime.now().minusDays(10);
+        File file = new File("pasta2/novo.txt");
+        boolean isCreated = file.createNewFile();
+        boolean isModified = file.setLastModified(date.toInstant(ZoneOffset.UTC).toEpochMilli());
+
+        Path path = Paths.get("pasta2/novo_path.txt");
+        if(Files.notExists(path)) {
+            Files.createDirectory(path);
+        }
+
+        FileTime fileTime = FileTime.from(date.toInstant(ZoneOffset.UTC));
+        Files.setLastModifiedTime(path, fileTime);
+        System.out.println(Files.isWritable(path));
+        System.out.println(Files.isReadable(path));
+        System.out.println(Files.isExecutable(path));
+    }
+
+}
