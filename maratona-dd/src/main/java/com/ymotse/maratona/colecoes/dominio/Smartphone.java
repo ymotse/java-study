6746947118d92d5d0bc49307b2101f271844ea81
ymotse <ymotse@gmail.com>
2021-09-16 13:50:14 -0300

    [maratona-dd] - Coleções - equals - 02

--- maratona-dd/src/main/java/com/ymotse/maratona/colecoes/dominio/Smartphone.java
+++ maratona-dd/src/main/java/com/ymotse/maratona/colecoes/dominio/Smartphone.java
@@ -9,6 +9,20 @@ public class Smartphone {
         this.marca = marca;
     }
 
+    // Reflexivo: x.equals(x) tem que ser TRUE para tudo que for diferente de NULL
+    // Simetrico: para x e y diferentes de NULL, se x.equals(y) == TRUE, logo, y.equals(x) == TRUE
+    // Transitividade: para x,y,z diferentes de NULL, se x.equals(y) == true, e y.equals(z) == TRUE, logo y.equals(z) == TRUE
+    // Consistente: x.equals(x) sempre retorna TRUE se x for diferente de NULL
+    // para x diferente de NULL, x.equals(NULL) tem que retornar FALSE.
+    @Override
+    public boolean equals(Object obj) {
+        if(obj == null) return false;
+        if(this == obj) return true;
+        if(this.getClass() != obj.getClass()) return false;
+        Smartphone smartphone = (Smartphone) obj;
+        return serialNumber != null && serialNumber.equals(smartphone.serialNumber);
+    }
+
     public String getSerialNumber() {
         return serialNumber;
     }
