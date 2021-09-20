b9d6a85a134273335ce012c3b4093b9ffa75463d
ymotse <ymotse@gmail.com>
2021-09-20 13:34:39 -0300

    [maratona-dd] - Coleções - Sorting List - 03 - Comparator

--- maratona-dd/src/main/java/com/ymotse/maratona/colecoes/execute/MangaSortTest01.java
+++ maratona-dd/src/main/java/com/ymotse/maratona/colecoes/execute/MangaSortTest01.java
@@ -4,7 +4,17 @@ import com.ymotse.maratona.colecoes.dominio.Manga;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+
+
+class MangaByIdComparator implements Comparator<Manga> {
+
+    @Override
+    public int compare(Manga manga1, Manga manga2) {
+        return manga1.getId().compareTo(manga2.getId());
+    }
+}
 
 public class MangaSortTest01 {
 
@@ -25,6 +35,17 @@ public class MangaSortTest01 {
         for(Manga manga : mangas) {
             System.out.println(manga);
         }
+        System.out.println("------------------");
+
+//        Collections.sort(mangas, new MangaByIdComparator());
+        mangas.sort(new MangaByIdComparator());
+
+        for(Manga manga : mangas) {
+            System.out.println(manga);
+        }
+
+
+
     }
 
 }
