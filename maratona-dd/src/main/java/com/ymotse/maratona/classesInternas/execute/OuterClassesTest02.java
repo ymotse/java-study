f56f4f3460af6f582f4c5221c3d56f6c69e7df51
ymotse <ymotse@gmail.com>
2021-09-28 09:12:01 -0300

    [maratona-dd] - Classes Internas - 02 - Classes Locais

---
+++ maratona-dd/src/main/java/com/ymotse/maratona/classesInternas/execute/OuterClassesTest02.java
@@ -0,0 +1,25 @@
+package com.ymotse.maratona.classesInternas.execute;
+
+public class OuterClassesTest02 {
+
+    private String name = "Midoriya";
+
+    void print() {
+
+        final String lastName = "Izuku";
+
+        class LocalClass {
+            public void printLocal() {
+                System.out.println(name + " " + lastName);
+            }
+        }
+
+        new LocalClass().printLocal();
+    }
+
+    public static void main(String[] args) {
+        OuterClassesTest02 outer = new OuterClassesTest02();
+        outer.print();
+    }
+
+}
