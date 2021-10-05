997d7a9b2e695d88834a9a8e375110399111c3b0
Yitshhaq <ymotse@gmail.com>
2021-10-05 14:15:18 -0300

    [maratona-dd] - Lambdas - 03 - Function

---
+++ maratona-dd/src/main/java/com/ymotse/maratona/comportamento/execute/LambdaTest02.java
@@ -0,0 +1,27 @@
+package com.ymotse.maratona.comportamento.execute;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+public class LambdaTest02 {
+
+    public static void main(String[] args) {
+        List<String> strings = List.of("Natsu", "Allucard");
+        List<Integer> lengthStringListElements = map(strings, (String s) -> s.length());
+        System.out.println(lengthStringListElements);
+
+        List<String> uppers = map(strings, s -> s.toUpperCase());
+        System.out.println(uppers);
+    }
+
+    private static <T, R> List<R> map(List<T> list, Function<T, R> function) {
+        List<R> resultado = new ArrayList<>();
+        for (T e : list) {
+            R r = function.apply(e);
+            resultado.add(r);
+        }
+        return resultado;
+    }
+
+}
