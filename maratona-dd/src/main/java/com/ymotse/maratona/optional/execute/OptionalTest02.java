f409e7f9e672151c1b60a60eb21b2134853151e5
Yitshhaq <ymotse@gmail.com>
2021-10-07 14:03:00 -0300

    [maratona-dd] - Optional - 02

---
+++ maratona-dd/src/main/java/com/ymotse/maratona/optional/execute/OptionalTest02.java
@@ -0,0 +1,23 @@
+package com.ymotse.maratona.optional.execute;
+
+import com.ymotse.maratona.optional.dominio.Manga;
+import com.ymotse.maratona.optional.repositorio.MangaRepository;
+
+import java.util.Optional;
+
+public class OptionalTest02 {
+
+    public static void main(String[] args) {
+        Optional<Manga> mangaByTitle = MangaRepository.findByTitle("Boku no Hero");
+        mangaByTitle.ifPresent(m -> m.setTitle("Boku no Hero 2"));
+        System.out.println(mangaByTitle);
+
+        Manga mangaById = MangaRepository.findById(2).orElseThrow(IllegalArgumentException::new);
+        System.out.println(mangaById);
+
+        Manga newManga = MangaRepository.findByTitle("Drifters")
+//                .orElse(new Manga(3, "Drifters", 20));
+                .orElseGet(() -> new Manga(3, "Drifters", 20));
+    }
+
+}
---
+++ maratona-dd/src/main/java/com/ymotse/maratona/optional/repositorio/MangaRepository.java
@@ -0,0 +1,34 @@
+package com.ymotse.maratona.optional.repositorio;
+
+import com.ymotse.maratona.optional.dominio.Manga;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
+
+public class MangaRepository {
+    private static List<Manga> mangas = List.of(
+            new Manga(1, "Boku no Hero", 58),
+            new Manga(2, "Overlord", 32)
+    );
+
+    public static Optional<Manga> findById(Integer id) {
+        return findBy(m -> m.getId().equals(id));
+    }
+
+    public static Optional<Manga> findByTitle(String title) {
+        return findBy(m -> m.getTitle().equals(title));
+    }
+
+    private static Optional<Manga> findBy(Predicate<Manga> predicate) {
+        Manga found = null;
+
+        for (Manga manga : mangas) {
+            if(predicate.test(manga)) {
+                found = manga;
+            }
+        }
+        return Optional.ofNullable(found);
+    }
+
+}
