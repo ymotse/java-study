9d0386e11fb6c6d6cd564f6d12eba91efa0fa646
Yitshhaq <ymotse@gmail.com>
2021-10-11 14:16:54 -0300

    [maratona-dd] - Streams - 03 - Introduction - 03

---
+++ maratona-dd/src/main/java/com/ymotse/maratona/streams/execute/StreamTest03.java
@@ -0,0 +1,40 @@
+package com.ymotse.maratona.streams.execute;
+
+import com.ymotse.maratona.streams.dominio.LightNovel;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class StreamTest03 {
+
+    private static List<LightNovel> lightNovels = new ArrayList<>(List.of(
+            new LightNovel("Tensei Shittara", 8.99),
+            new LightNovel("Overlord", 3.99),
+            new LightNovel("Violet Evergarden", 5.99),
+            new LightNovel("No Game no life", 2.99),
+            new LightNovel("Fullmetal Alchemist", 5.99),
+            new LightNovel("Kumo Desuga", 1.99),
+            new LightNovel("Kumo Desuga", 1.99),
+            new LightNovel("Monogatari", 4.00)
+    ));
+
+    public static void main(String[] args) {
+
+        Stream<LightNovel> stream = lightNovels.stream();
+        lightNovels.forEach(System.out::println);
+
+        long count = stream
+                .filter(ln -> ln.getPrice() <= 4)
+                .count();
+
+        long count2 = lightNovels.stream()
+                .distinct()
+                .filter(ln -> ln.getPrice() <= 4)
+                .count();
+
+        System.out.println(count);
+        System.out.println(count2);
+    }
+
+}
