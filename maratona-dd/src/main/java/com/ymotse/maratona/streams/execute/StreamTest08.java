b432caa5b47cbd7434c5a2e8551acb5811234faf
Yitshhaq <ymotse@gmail.com>
2021-10-14 13:17:03 -0300

    [maratona-dd] - Streams - 08 - Reduce - 02

---
+++ maratona-dd/src/main/java/com/ymotse/maratona/streams/execute/StreamTest08.java
@@ -0,0 +1,36 @@
+package com.ymotse.maratona.streams.execute;
+
+import com.ymotse.maratona.streams.dominio.LightNovel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StreamTest08 {
+
+    private static List<LightNovel> lightNovels = new ArrayList<>(List.of(
+            new LightNovel("Tensei Shittara", 8.99),
+            new LightNovel("Overlord", 10.99),
+            new LightNovel("Violet Evergarden", 5.99),
+            new LightNovel("No Game no life", 2.99),
+            new LightNovel("Fullmetal Alchemist", 5.99),
+            new LightNovel("Kumo Desuga", 1.99),
+            new LightNovel("Kumo Desuga", 1.99),
+            new LightNovel("Monogatari", 4.00)
+    ));
+
+    public static void main(String[] args) {
+        lightNovels.stream()
+                .map(LightNovel::getPrice)
+                .filter(price -> price > 3)
+                .reduce(Double::sum)
+                .ifPresent(System.out::println);
+
+        double sum = lightNovels.stream()
+                .mapToDouble(LightNovel::getPrice)
+                .filter(price -> price > 3)
+                .sum();
+
+        System.out.println(sum);
+    }
+
+}
