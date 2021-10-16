7f4b962b3995a9ee4dd81864341a33f4a1d5a355
Yitshhaq <ymotse@gmail.com>
2021-10-16 09:43:24 -0300

    [maratona-dd] - Threads - 05 - Sincronismo de thread - 01

---
+++ maratona-dd/src/main/java/com/ymotse/maratona/threads/dominio/Account.java
@@ -0,0 +1,14 @@
+package com.ymotse.maratona.threads.dominio;
+
+public class Account {
+
+    private int balance = 50;
+
+    public void withdrawal(int amount) {
+        this.balance = this.balance - amount;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+}
