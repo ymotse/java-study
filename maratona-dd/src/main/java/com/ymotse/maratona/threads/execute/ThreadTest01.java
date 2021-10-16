e5ffa09eac56b33e24553bdd82ad589d783af882
Yitshhaq <ymotse@gmail.com>
2021-10-16 09:15:50 -0300

    [maratona-dd] - Threads - 03 - Prioridade e Sleep

--- maratona-dd/src/main/java/com/ymotse/maratona/threads/execute/ThreadTest01.java
+++ maratona-dd/src/main/java/com/ymotse/maratona/threads/execute/ThreadTest01.java
@@ -34,6 +34,11 @@ class ThreadExampleRunnable implements Runnable {
             if(i % 100 == 0) {
                 System.out.println();
             }
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
@@ -47,10 +52,11 @@ public class ThreadTest01 {
 //        ThreadExample t3 = new ThreadExample('C');
 //        ThreadExample t4 = new ThreadExample('D');
 
-        Thread t1 = new Thread(new ThreadExampleRunnable('A'));
-        Thread t2 = new Thread(new ThreadExampleRunnable('B'));
-        Thread t3 = new Thread(new ThreadExampleRunnable('C'));
-        Thread t4 = new Thread(new ThreadExampleRunnable('D'));
+        Thread t1 = new Thread(new ThreadExampleRunnable('A'), "T1A");
+        Thread t2 = new Thread(new ThreadExampleRunnable('B'), "T2B");
+        Thread t3 = new Thread(new ThreadExampleRunnable('C'), "T3C");
+        Thread t4 = new Thread(new ThreadExampleRunnable('D'), "T4D");
+        t4.setPriority(Thread.MAX_PRIORITY);
         t1.start();
         t2.start();
         t3.start();
