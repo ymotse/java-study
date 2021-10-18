b402e2cf593a3708be4e967e00c0f0eb497c5db5
Yitshhaq <ymotse@gmail.com>
2021-10-18 14:16:46 -0300

    [maratona-dd] - Threads - 07 - Sincronismo de thread - 03 - Classes thread safe

---
+++ maratona-dd/src/main/java/com/ymotse/maratona/threads/execute/ThreadSafeTest01.java
@@ -0,0 +1,33 @@
+package com.ymotse.maratona.threads.execute;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+class ThreadSafeNames {
+//    private final List<String> names = Collections.synchronizedList(new ArrayList<>());
+    private final List<String> names = new ArrayList<>();
+
+    public synchronized void add(String name) {
+        names.add(name);
+    }
+
+    public synchronized void removeFirst() {
+        if(names.size() > 0) {
+            System.out.println(Thread.currentThread().getName());
+            System.out.println(names.remove(0));
+        }
+    }
+}
+
+public class ThreadSafeTest01 {
+
+    public static void main(String[] args) {
+        ThreadSafeNames threadSafeNames = new ThreadSafeNames();
+        threadSafeNames.add("Goku");
+        Runnable r = threadSafeNames::removeFirst;
+        new Thread(r).start();
+        new Thread(r).start();
+    }
+
+}
