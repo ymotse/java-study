f2231970ffbfac2e7c388552b17125487edfa254
Yitshhaq <ymotse@gmail.com>
2021-11-03 09:25:56 -0300

    [maratona-dd] - Concorrência - 14 - CompletableFuture - 03 - ThreadFactory

---
+++ maratona-dd/src/main/java/com/ymotse/maratona/concorrencia/execute/CompletableFutureTest03.java
@@ -0,0 +1,41 @@
+package com.ymotse.maratona.concorrencia.execute;
+
+import com.ymotse.maratona.concorrencia.service.StoreServiceDeprecated;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class CompletableFutureTest03 {
+
+    public static void main(String[] args) {
+        StoreServiceDeprecated storeServiceDeprecated = new StoreServiceDeprecated();
+        searchPricesAsyncCompletableFuture(storeServiceDeprecated);
+    }
+
+    private static void searchPricesAsyncCompletableFuture(StoreServiceDeprecated storeServiceDeprecated) {
+        long start = System.currentTimeMillis();
+        ExecutorService executor = Executors.newFixedThreadPool(10, r -> {
+            Thread thread = new Thread(r);
+            thread.setDaemon(true);
+            return thread;
+        });
+
+        List<String> stores = List.of("Store 1", "Store 2", "Store 3", "Store 4");
+        List<CompletableFuture<Double>> completableFutures = stores.stream()
+                .map(s -> CompletableFuture.supplyAsync(() -> storeServiceDeprecated.getPriceSync(s), executor))
+                .collect(Collectors.toList());
+
+        List<Double> prices = completableFutures.stream()
+                .map(CompletableFuture::join)
+                .collect(Collectors.toList());
+
+        System.out.println(prices);
+        long end = System.currentTimeMillis();
+        System.out.printf("Time passed to searchPricesAsyncCompletableFuture %dms%n", (end-start));
+    }
+
+}
