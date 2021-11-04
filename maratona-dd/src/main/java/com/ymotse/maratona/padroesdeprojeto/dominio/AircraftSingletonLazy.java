465ddd58dc5f89838ac26e71e23caff709e0fafa
Yitshhaq <ymotse@gmail.com>
2021-11-04 14:17:51 -0300

    [maratona-dd] - Padrões de Projeto - 04 - Singleton - 02 - Lazy Initialization

---
+++ maratona-dd/src/main/java/com/ymotse/maratona/padroesdeprojeto/dominio/AircraftSingletonLazy.java
@@ -0,0 +1,37 @@
+package com.ymotse.maratona.padroesdeprojeto.dominio;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public final class AircraftSingletonLazy {
+    // Lazy initialization
+    private static AircraftSingletonLazy INSTANCE;
+
+    private final Set<String> availableSeats = new HashSet<>();
+    private final String name;
+
+    private AircraftSingletonLazy(String name) {
+        this.name = name;
+    }
+
+    {
+        availableSeats.add("1A");
+        availableSeats.add("1B");
+    }
+
+    public static AircraftSingletonLazy getINSTANCE() {
+        if(INSTANCE == null) {
+            synchronized (AircraftSingletonLazy.class) {
+                if(INSTANCE == null) {
+                    INSTANCE = new AircraftSingletonLazy("878-901");
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+    public boolean bookSeat(String seat) {
+        return availableSeats.remove(seat);
+    }
+
+}
---
+++ maratona-dd/src/main/java/com/ymotse/maratona/padroesdeprojeto/execute/AircraftSingletonLazyTest01.java
@@ -0,0 +1,28 @@
+package com.ymotse.maratona.padroesdeprojeto.execute;
+
+import com.ymotse.maratona.padroesdeprojeto.dominio.AircraftSingletonLazy;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+public class AircraftSingletonLazyTest01 {
+
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        bookSeat("1A");
+        bookSeat("1A");
+        System.out.println(AircraftSingletonLazy.getINSTANCE());
+        System.out.println(AircraftSingletonLazy.getINSTANCE());
+
+        Constructor<AircraftSingletonLazy> constructor = AircraftSingletonLazy.class.getDeclaredConstructor(String.class);
+        constructor.setAccessible(true);
+        AircraftSingletonLazy aircraftSingletonLazy = constructor.newInstance("878-901");
+        System.out.println(aircraftSingletonLazy);
+    }
+
+    private static void bookSeat(String seat) {
+        System.out.println(AircraftSingletonLazy.getINSTANCE());
+        AircraftSingletonLazy aircraft = AircraftSingletonLazy.getINSTANCE();
+        System.out.println(aircraft.bookSeat(seat));
+    }
+
+}
