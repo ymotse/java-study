2253ac8904eb33eac0c78eec66000e8f8463ff3b
Yitshhaq <ymotse@gmail.com>
2021-11-09 09:23:02 -0300

    [maratona-dd] - JDBC - 09 - Atualizando dados com Statement

--- maratona-dd/src/main/java/com/ymotse/maratona/jdbc/execute/ConnectionFactoryTest01.java
+++ maratona-dd/src/main/java/com/ymotse/maratona/jdbc/execute/ConnectionFactoryTest01.java
@@ -7,6 +7,8 @@ public class ConnectionFactoryTest01 {
     public static void main(String[] args) {
 //        Producer producer = Producer.builder().name("Universal Studio").build();
 //        ProducerService.save(producer);
-        ProducerService.delete(6);
+//        ProducerService.delete(6);
+        Producer producerToUpdate = Producer.builder().id(1).name("MADHOUSE").build();
+        ProducerService.update(producerToUpdate);
     }
 }
