048804dcd51c3c878de6dac1e349ac0961e4e985
Yitshhaq Fukushima <ymotse@gmail.com>
2022-04-14 16:49:44 -0300

    [maratona-jsf] - Expression Language - Objetos e enumerações

--- maratona-jsf/src/main/java/bean/estudante/EstudanteRegistrarBean.java
+++ maratona-jsf/src/main/java/bean/estudante/EstudanteRegistrarBean.java
@@ -4,44 +4,20 @@ import java.io.Serializable;
 
 import javax.inject.Named;
 
+import model.Estudante;
+
 @Named("umNomeMaluco")
 public class EstudanteRegistrarBean implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
-	private String nome = "Yitshhaq";
-	private String sobrenome = "Fukushima";
-	private double nota1 = 10;
-	private double nota2;
-	private double nota3 = 10;
+	private Estudante estudante = new Estudante();
+
+    public Estudante getEstudante() {
+        return estudante;
+    }
+
+    public void setEstudante(Estudante estudante) {
+        this.estudante = estudante;
+    }
 	
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	public String getSobrenome() {
-		return sobrenome;
-	}
-	public void setSobrenome(String sobrenome) {
-		this.sobrenome = sobrenome;
-	}
-	public double getNota1() {
-		return nota1;
-	}
-	public void setNota1(double nota1) {
-		this.nota1 = nota1;
-	}
-	public double getNota2() {
-		return nota2;
-	}
-	public void setNota2(double nota2) {
-		this.nota2 = nota2;
-	}
-	public double getNota3() {
-		return nota3;
-	}
-	public void setNota3(double nota3) {
-		this.nota3 = nota3;
-	}
 }
